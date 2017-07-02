d0bf17ee1b438d11c2b3a6c444fa4d568f29bd22
Boutérâa Belhassen Chedly <belha99@hotmail.fr>
2017-07-02 15:09:16 +0100

    t

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ var longest = require("./Get_longest_prefix.js");
 var cheapest = require("./Cheapest_operator.js");
 
 // RAW DATA FOR TESTING 
-var OperatorA_price_list = [['1', '0.9'],['268', '5.1'],['46', '0.17'],['4620', '0.0'],['468', '0.15'],['4631', '0.15'],['4673', '0.9'],['46732', '1.1']];
-var OperatorB_price_list = [['1', '0.92'],['44', '0.5'],['46', '0.2'],['467', '1.0'],['48', '1.2']];
+var OperatorA_price_list = [['1', '0.9'], ['268', '5.1'], ['46', '0.17'], ['4620', '0.0'], ['468', '0.15'], ['4631', '0.15'], ['4673', '0.9'], ['46732', '1.1']];
+var OperatorB_price_list = [['1', '0.92'], ['44', '0.5'], ['46', '0.2'], ['467', '1.0'], ['48', '1.2']];
 
 // NUMBER FOR TESTING
-const challenge_Number = "4673212345" ; //68123456789 4673212345
+var challenge_Number = "4673212345"; //68123456789 4673212345
 
-var prefixes_found = [] ;
+var prefixes_found = [];
 var prefix_selected = "";
-var couple = {}         ;
-var couples = []        ;
-var rooting_to = ""     ;
+var couple = {};
+var couples = [];
+var rooting_to = "";
 
 var nothing_found = true;
 
@@ -53,6 +53,9 @@ for (var i = 0; i <= OperatorA_price_list.length -1; i++ ){
     }
 }
 
-//exporting the phone number and the price list to be tested
+//exporting the phone number and the price list (operatorA only) to be tested
 exports.Dialed_number = challenge_Number ;
 exports.price_list = pl ;
+exports.any_price_list = OperatorA_price_list ;
+exports.All_operators = All_operators ;
+exports.nothing_found = nothing_found ;
--- src/main.test.js
+++ src/main.test.js
@@ -1,7 +1,11 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
+
 var Dialed_number = require('./main.js').Dialed_number;
 var pl = require('./main.js').price_list;
+var any_price_list = require('./main.js').any_price_list;
+var All_ops = require('./main.js').All_operators;
+var nothing_found = require('./main.js').nothing_found;
 
 describe('rooting-of-telephone-calls', function(){
    
@@ -29,6 +33,23 @@ describe('rooting-of-telephone-calls', function(){
                 
         });
         
+        it("each price_list should have its operator name", ()=>{
+            
+            expect(any_price_list).to.have.property('name');
+            
+        });
+        
+        it("All price lists are pushed in one array [] to loop throuh them all", ()=>{
+            
+            expect(All_ops).to.be.an('array');
+            
+        });
+        
+        it('initially there is no prefixes found for a given number', ()=>{
+            
+            assert.isTrue(nothing_found, "should be true");
+            
+        });
     });
  
 });
